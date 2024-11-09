d16ecf6c078a2435edd100efc154cebf208d9c4f
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-11-10 00:59:08 +0300

    Initial Commit

---
+++ GoRoute/GoRouteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoRouteApp.swift
+//  GoRoute
+//
+//  Created by Александр Московцев on 10.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct GoRouteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
