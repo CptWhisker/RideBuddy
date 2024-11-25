e184dc77bebc611037d5fb3787b35e2d8ee79b5a
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-11-26 00:24:03 +0300

    Reorganize codebase
    - Moved all content related to testing the Yandex Schedule API to a separate view.

--- GoRoute/MainScreen/View/MainScreen.swift
+++ GoRoute/MainScreen/View/MainScreen.swift
@@ -12,49 +12,10 @@ struct MainScreen: View {
     @ObservedObject var viewModel: MainScreenViewModel
     
     var body: some View {
-        ZStack {
-            Color.gray
-                .ignoresSafeArea()
-                .opacity(0.25)
-            
-            VStack(spacing: 20) {
-                ForEach(MainScreenAction.allCases) { action in
-                    MainScreenButton(title: action.rawValue) {
-                        Task {
-                            do {
-                                try await action.performAction(on: viewModel)
-                            } catch {
-                                print(action.errorDescription, error.localizedDescription)
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        Text("Main Screen")
     }
 }
 
 #Preview {
     MainScreen(viewModel: MainScreenViewModel())
-}
-
-private extension MainScreen {
-    
-    struct MainScreenButton: View {
-        
-        let title: String
-        let action: () -> Void
-        
-        var body: some View {
-            Button(action: action) {
-                Text(title)
-                    .font(.headline)
-                    .frame(width: 300, height: 60)
-                    .background(.blue)
-                    .foregroundStyle(.white)
-                    .clipShape(.rect(cornerRadius: 10))
-                    .shadow(radius: 5)
-            }
-        }
-    }
 }
---
+++ GoRoute/Utilities/Testing/YandexScheduleTestsView.swift
@@ -0,0 +1,61 @@
+//
+//  YandexScheduleTestsView.swift
+//  GoRoute
+//
+//  Created by Aleksandr Moskovtsev on 26.11.2024.
+//
+
+import SwiftUI
+
+struct YandexScheduleTestsView: View {
+    
+    @ObservedObject var viewModel: MainScreenViewModel
+    
+    var body: some View {
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+                .opacity(0.25)
+            
+            VStack(spacing: 20) {
+                ForEach(MainScreenAction.allCases) { action in
+                    YandexScheduleTestsButton(title: action.rawValue) {
+                        Task {
+                            do {
+                                try await action.performAction(on: viewModel)
+                            } catch {
+                                print(action.errorDescription, error.localizedDescription)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+private extension YandexScheduleTestsView {
+    
+    struct YandexScheduleTestsButton: View {
+        
+        let title: String
+        let action: () -> Void
+        
+        var body: some View {
+            Button(action: action) {
+                Text(title)
+                    .font(.headline)
+                    .frame(width: 300, height: 60)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 10))
+                    .shadow(radius: 5)
+            }
+        }
+    }
+}
+
+
+#Preview {
+    YandexScheduleTestsView(viewModel: MainScreenViewModel())
+}
