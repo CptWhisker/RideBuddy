3407fcfc4ffeb9eef5fc007522c049f1124ca660
Aleksandr Moskovtsev <aleks.moskovtsev@gmail.com>
2024-12-04 09:44:52 +0300

    Create `ErrorView`
    - Created `ErrorView` to display potential errors;
    - Added error images to the asset catalogue.

---
+++ GoRoute/Views/ErrorView.swift
@@ -0,0 +1,28 @@
+//
+//  ErrorView.swift
+//  GoRoute
+//
+//  Created by Aleksandr Moskovtsev on 04.12.2024.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    
+    let image: ImageResource
+    let title: String
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Image(image)
+            
+            Text(title)
+                .font(.system(size: 24, weight: .bold))
+                .foregroundStyle(.accent)
+        }
+    }
+}
+
+#Preview {
+    ErrorView(image: .noInternet, title: "Нет интернета")
+}
